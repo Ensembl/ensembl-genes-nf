c57b887eb9912cea99de32e228466693004f0d75
ens-ftricomi <ftricomi@ebi.ac.uk>
2022-08-02 16:13:15 +0100

    added script to get the busco set from meta table

--- supplementary_files/get_busco_dataset.sh
+++ supplementary_files/get_busco_dataset.sh
@@ -1,36 +1,18 @@
-user=ensro
-
-host=mysql-ens-sta-5
-
-port=4684
-db=$1
-file=/nfs/production/flicek/ensembl/genebuild/ftricomi/nextflow/busco_dataset.txt
+user=$1
+host=$2
+port=$3
+dbname=$4
+file=$ENSCODE/ensembl-genes-nf/supplementary_files/busco_dataset.txt
 #mysql-ens-sta-5,4684,ensro,canis_lupus_gca905319855v2_core_106_1,mysql-ens-sta-5,4684,ensro,canis_lupus_gca905319855v2_core_106_1
+#match the first correspondence between the list of metavalues and the list of busco datasets
 mapfile result < <(mysql -N -u $user  -h $host -P $port -D $db -se'select meta_value from meta where meta_key="species.classification" order by meta_id')
-#echo ${result[2]}
 tentative=0
 for i in "${result[@]}";
 do      
-
 	species=$(echo "$i" | tr '[:upper:]' '[:lower:]')	
 	if grep $species $file;then
-	
-	  #if  $tentative -ne 2;then 
-      	    
-   #a=$(grep $i $file)
-   #echo grep "$i" $file
             echo $i
-	    #$tentative=$tentative+1
-            echo $db	$(grep $species $file) >> test.txt
+            #echo $db	$(grep $species $file) >> test.txt
             break;
-         #fi
     fi
-        
-
 done
-
-
-#string='My long string'
-#if [[ $string == *"My long"* ]]; then
-#  echo "It's there!"
-#fi
