83704eda7d7adfe1ee037890e51e8948729e2b69
William Stark <william@ebi.ac.uk>
2023-02-14 13:24:54 +0000

    add copyright notice and ability to gracefully halt execution

--- annotation_stats/tasks.py
+++ annotation_stats/tasks.py
@@ -2,6 +2,22 @@
 # -*- coding: utf-8 -*-
 
 
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """
 Task functions for the annotation stats Nextflow pipeline.
 """
@@ -26,4 +42,7 @@ def check_stats_files(annotation_directory: str, production_name: str):
 
 
 if __name__ == "__main__":
-    fire.Fire()
+    try:
+        fire.Fire()
+    except KeyboardInterrupt:
+        print("Interrupted with CTRL-C, exiting...")
