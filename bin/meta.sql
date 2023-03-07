debfeb49b123af72f41b788c7350b6f2235e021c
ens-ftricomi <ftricomi@ebi.ac.uk>
2023-03-07 12:21:07 +0000

    REFACTORING - moved supplementary files to bin

---
+++ bin/meta.sql
@@ -0,0 +1,1 @@
+select replace(group_concat(meta_value order by field(meta_key,'species.scientific_name','assembly.accession','species.annotation_source') SEPARATOR '/'), ' ','_') from meta where meta_key in ('species.scientific_name','assembly.accession','species.annotation_source');
