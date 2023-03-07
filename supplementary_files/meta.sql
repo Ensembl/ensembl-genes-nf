9d8d2e4f7d08bfbe204eb71fa862785b107399d3
ens-ftricomi <ftricomi@ebi.ac.uk>
2023-03-07 12:18:36 +0000

    REFACTORING - cleaning

--- supplementary_files/meta.sql
+++
@@ -1,2 +0,0 @@
-select replace(group_concat(meta_value order by field(meta_key,'species.scientific_name','assembly.accession','species.annotation_source') SEPARATOR '/'), ' ','_') from meta where meta_key in ('species.scientific_name','assembly.accession','species.annotation_source');
-
